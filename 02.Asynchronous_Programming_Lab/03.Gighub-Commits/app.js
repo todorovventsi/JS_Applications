da31a3d653a97caf01fd553c08ff13c2477000b0
Ventsislav Todorov <todorov.ventsi@gmail.com>
2022-10-27 16:05:36 +0300

    ...

--- 02.Asynchronous_Programming_Lab/03.Gighub-Commits/app.js
+++ 02.Asynchronous_Programming_Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,33 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+    const username = document.getElementById("username").value;
+    const repo = document.getElementById("repo").value;
+    const commitsField = document.getElementById("commits");
+
+    const queryURL = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+    commitsField.innerHTML = "";
+
+    try {
+        let response = await fetch(queryURL);
+
+        if (response.ok == false) {
+            throw new Error("Not Found");
+        }
+
+        let commits = await response.json();
+
+        for (let entry of commits) {
+            let { commit } = entry;
+            let authorName = commit.author.name;
+            let message = commit.message;
+            let liItem = document.createElement("li");
+            liItem.textContent = `${authorName}: ${message}`;
+
+            commitsField.appendChild(liItem);
+        }
+    } catch (error) {
+        let liItem = document.createElement("li");
+        liItem.textContent = error.message;
+        commitsField.appendChild(liItem);
+    }
 }
