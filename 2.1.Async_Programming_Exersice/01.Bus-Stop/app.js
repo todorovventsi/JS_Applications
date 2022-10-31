b7c296093161002c259af5b3684f0ea5ada78334
Ventsislav Todorov <todorov.ventsi@gmail.com>
2022-10-31 13:11:11 +0200

    ...

--- 2.1.Async_Programming_Exersice/01.Bus-Stop/app.js
+++ 2.1.Async_Programming_Exersice/01.Bus-Stop/app.js
@@ -1,3 +1,28 @@
 function getInfo() {
-    console.log("TODO...");
+    // Get element referrences
+    // fetch db
+    // using promise chain create li tags
+    // append li tags to ul
+    // handle errors
+
+    const inputField = document.getElementById("stopId");
+    const stopNameDiv = document.getElementById("stopName");
+    const busesUl = document.getElementById("buses");
+    const baseURL = `http://localhost:3030/jsonstore/bus/businfo`;
+
+    fetch(`${baseURL}/${inputField.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+            stopNameDiv.textContent = data.name;
+            busesUl.innerHTML = "";
+            Object.keys(data.buses).forEach((busNumber) => {
+                let liItem = document.createElement("li");
+                liItem.textContent = `Bus ${busNumber} arrives in ${data.buses[busNumber]} minutes`;
+                busesUl.appendChild(liItem);
+            });
+        })
+        .catch((err) => {
+            stopNameDiv.textContent = "Error";
+            busesUl.innerHTML = "";
+        });
 }
